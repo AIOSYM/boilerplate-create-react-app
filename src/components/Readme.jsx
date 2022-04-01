b0319444b75456e16df838e7b8cb84a748c55d25
David Pich <davidpich.kh@gmail.com>
2022-04-02 05:24:10 +0900

    Setup tailwind, daisyUI

---
+++ src/components/Readme.jsx
@@ -0,0 +1,14 @@
+
+function Readme() {
+  return (
+    <div className="p-4">
+    <h1 className="text-5xl font-bold text-primary">Create React App Boilerplate</h1>
+    <h3 className="text-xl">With below setup:</h3>
+    <ul>
+      <li>Tailwind CSS + DaisyUI</li>
+    </ul>
+  </div>
+  )
+}
+
+export default Readme
